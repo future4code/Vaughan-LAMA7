760e06835fdc5603acd9a9796a29c103a502f65d
Sofia Pitta Sesso <sofit_sops@hotmail.com>
2022-05-12 20:01:46 -0300

    Models

---
+++ src/model/Band.ts
@@ -0,0 +1,8 @@
+export class Band {
+    constructor(
+        private id: string,
+        private name: string,
+        private musicGenre: string,
+        private responsible: string
+    ) { }
+}
---
+++ src/model/User.ts
@@ -0,0 +1,11 @@
+import { USER_ROLES } from "../types/USER_ROLES"
+
+export class User {
+    constructor(
+        private id: string,
+        private name: string,
+        private email: string,
+        private password: string,
+        private role: USER_ROLES
+    ) { }
+}
