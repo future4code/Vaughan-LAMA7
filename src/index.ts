5162932b65a602949060e5775721cca0e9515aa7
udimile <76756821+udimile@users.noreply.github.com>
2022-05-12 18:56:49 -0300

    index

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, {Express} from 'express'
+import cors from 'cors'
+import { AddressInfo } from "net";
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/bands", bandRouter)
+app.use("/users", userRouter)
+app.use("/shows", showRouter)
+
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+       const address = server.address() as AddressInfo;
+       console.log(`Servidor funcionando no http://localhost:${address.port}`);
+    } else {
+       console.error(`Falha na inicialização do servidor.`);
+    }
+});
