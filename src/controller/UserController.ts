8ab24749cf9c039a8ad200c203e56851fee1fbae
udimile <76756821+udimile@users.noreply.github.com>
2022-05-13 14:09:32 -0300

    login (#10)

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { UserBusiness } from "../business/UserBusiness";
 import { CustomError } from "../error/CustomError";
-import { SignupInputDTO } from "../model/User";
+import { LoginInputDTO, SignupInputDTO } from "../model/User";
 
 export class UserController {
     constructor(
@@ -29,6 +29,28 @@ export class UserController {
             } else if (error instanceof Error) {
                 res.status(400).send(error.message)
             }
+        }
+    }
+
+    public login = async (req: Request, res: Response): Promise<void> => {
+        try{
+            const{email, password} = req.body
+
+            const input: LoginInputDTO = {
+                email,
+                password
+            }
+            const token: string = await this.userBusiness.login(input)
+
+            res.status(200).send({token, message: "Login realizado com sucesso!"})
+        } 
+        catch(error: any){
+            if (error instanceof CustomError) {
+                res.status(error.statusCode).send(error.message)
+            } else if (error instanceof Error) {
+                res.status(400).send(error.message)
+            }
+
         }
     }
 
